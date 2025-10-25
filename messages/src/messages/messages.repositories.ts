73a018eb7d3bf930579710a1206032a91d1ee068
Sarah O. <sarah.shofani@proton.me>
2025-10-25 11:10:44 -0300

    Implementing a Service

--- messages/src/messages/messages.repositories.ts
+++ messages/src/messages/messages.repositories.ts
@@ -5,7 +5,7 @@ type messageObj = {
   id: number;
 };
 
-export class MessageRepository {
+export class MessagesRepository {
   async findOne(id: string | number): Promise<messageObj> {
     const content = await readFile('messages.json', 'utf8');
     const messages = JSON.parse(content);
