02229b16a7b6489ce059c0df11550037943f68b1
Sarah O. <sarah.shofani@proton.me>
2025-10-25 12:57:51 -0300

    Consuming Multiple Modules

--- dependency-injection/src/computer/computer.module.ts
+++ dependency-injection/src/computer/computer.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 
-import { ComputerController } from './computer.controller';
 import { CpuModule } from 'src/cpu/cpu.module';
 import { DiskModule } from 'src/disk/disk.module';
+import { ComputerController } from './computer.controller';
 
 @Module({
   imports: [CpuModule, DiskModule],
