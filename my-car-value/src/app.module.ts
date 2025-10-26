4298865590c520908cee8cb6354204d1ed6af093
Sarah O. <sarah.shofani@proton.me>
2025-10-26 15:14:48 -0300

    Viewing a DB's Contents

--- my-car-value/src/app.module.ts
+++ my-car-value/src/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Report } from './reports/report.entity';
 import { ReportsModule } from './reports/reports.module';
-import { UsersModule } from './users/users.module';
 import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -14,7 +15,7 @@ import { User } from './users/user.entity';
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [User],
+      entities: [User, Report],
       synchronize: true,
     }),
   ],
