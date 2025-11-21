012a526678ba8728a4aebb89381feac5e01b15e7
Sarah O. <sarah.shofani@proton.me>
2025-11-21 16:22:07 -0300

    Refactoring to Use Dependency Injection

--- messages/src/messages/messages.module.ts
+++ messages/src/messages/messages.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+
+import { MessagesRepository } from './message.repository';
 import { MessagesController } from './messages.controller';
+import { MessagesService } from './messages.service';
 
 @Module({
-  controllers: [MessagesController]
+  controllers: [MessagesController],
+  providers: [MessagesRepository, MessagesService],
 })
 export class MessagesModule {}
