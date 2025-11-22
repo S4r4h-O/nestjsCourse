6335c80813bef9ba1bf88ffff9714cdb46faf388
Sarah O. <sarah.shofani@proton.me>
2025-11-22 12:12:31 -0300

    Creating an Entity and Repository

--- my-car-value/src/app.module.ts
+++ my-car-value/src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportsModule } from './reports/reports.module';
+import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -11,7 +12,7 @@ import { UsersModule } from './users/users.module';
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [],
+      entities: [User],
       synchronize: true,
     }),
     ReportsModule,
