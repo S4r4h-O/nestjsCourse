fc218e23464e0dfb2d293b74f4c9aa1014e7fff2
Sarah O. <sarah.shofani@proton.me>
2025-11-23 16:50:30 -0300

    Controller-Wide Serialization

--- my-car-value/src/users/users.controller.ts
+++ my-car-value/src/users/users.controller.ts
@@ -15,6 +15,7 @@ import { CreateUserDto, UpdateUserDto, UserDto } from './dtos';
 import { UsersService } from './users.service';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
@@ -24,7 +25,6 @@ export class UsersController {
   }
 
   // UseInterceptor(new SerializeInterceptor(UserDto))
-  @Serialize(UserDto)
   @Get(':id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
@@ -35,7 +35,6 @@ export class UsersController {
   }
 
   // UseInterceptor(new SerializeInterceptor(UserDto))
-  @Serialize(UserDto)
   @Get()
   findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
