0528bc9b0ec2d8be6e03c177860132e242ef93bc
Sarah O. <sarah.shofani@proton.me>
2025-11-23 19:23:55 -0300

    Salting and Hashing the Password

--- my-car-value/src/users/auth.service.ts
+++ my-car-value/src/users/auth.service.ts
@@ -1,6 +1,10 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { randomBytes, scrypt as _script } from 'crypto';
+import { promisify } from 'util';
 
 import { UsersService } from './users.service';
+
+const scrypt = promisify(_script);
 
 @Injectable()
 export class AuthService {
@@ -13,7 +17,12 @@ export class AuthService {
       throw new BadRequestException('Email in use');
     }
     // Hash user's password
+    // generate a salt
+    const salt = randomBytes(8).toString('hex');
+    // Hash the salt and password together
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
     // Create new user and save it
+    const result = salt + '.' + hash.toString('hex');
     // return the user
   }
 
