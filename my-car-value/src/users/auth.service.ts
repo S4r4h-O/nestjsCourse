7aec416544bd26eaba78a2d553c550b194d13122
Sarah O. <sarah.shofani@proton.me>
2025-11-23 17:19:21 -0300

    Implementing Signup Functionality

--- my-car-value/src/users/auth.service.ts
+++ my-car-value/src/users/auth.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 
 import { UsersService } from './users.service';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
+
+  async signUp(email: string, password: string) {
+    // See if email is in use
+    const users = await this.usersService.find(email);
+    if (users.length) {
+      throw new BadRequestException('Email in use');
+    }
+    // Hash user's password
+    // Create new user and save it
+    // return the user
+  }
+
+  signIn() {}
 }
