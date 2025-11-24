8c35a4ce9a3ea32f062b644c1e331a0c8848a655
Sarah O. <sarah.shofani@proton.me>
2025-11-24 10:44:19 -0300

    Communicating from Interceptor to Decorator

--- my-car-value/src/users/decorators/current-user.decorator.ts
+++ my-car-value/src/users/decorators/current-user.decorator.ts
@@ -4,6 +4,9 @@ export const CurrentUser = createParamDecorator(
   (data: any, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
     console.log(request.session.userId);
+    console.log('cookies:', context.switchToHttp().getRequest().headers.cookie);
+    console.log('session:', context.switchToHttp().getRequest().session);
+
     return 'hi there';
   },
 );
