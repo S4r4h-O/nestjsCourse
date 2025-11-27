b714d778903afe55a82247e3a086070a19f3e47e
Sarah O. <sarah.shofani@proton.me>
2025-11-27 14:56:02 -0300

    Not Super Effective Tests

--- my-car-value/src/users/users.controller.spec.ts
+++ my-car-value/src/users/users.controller.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { AuthService } from './auth.service';
@@ -51,5 +52,21 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('findAllUsers returns a list of users with the given email', async () => {
+    const users = await controller.findAllUsers('users@email.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('users@email.com');
+  });
+
+  it('findUser returns a single user with a given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+  });
+
+  it('findUser returns an error if user with give id is not found', async () => {
+    fakeUsersServices.findOne = () => null;
+    await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
   });
 });
