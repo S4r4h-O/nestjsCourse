d5fbce87db184e016f10f4d1fc998ecbf36e77b8
Sarah O. <sarah.shofani@proton.me>
2025-11-27 12:14:32 -0300

    Unit Testing a Controller

--- my-car-value/src/users/users.controller.spec.ts
+++ my-car-value/src/users/users.controller.spec.ts
@@ -1,10 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUsersServices: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    fakeUsersServices = {
+      findOne: () => {},
+      find: () => {},
+      remove: () => {},
+      update: () => {},
+    };
+    fakeAuthService = {
+      signUp: () => {},
+      signIn: () => {},
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
     }).compile();
