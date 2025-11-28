39fa7ee5ba18f8a6906b87ba8e1f7526d9c2b08f
Sarah O. <sarah.shofani@proton.me>
2025-11-28 11:45:22 -0300

    Transforming Query String Data

--- my-car-value/src/reports/dtos/get-estimate.dto.ts
+++ my-car-value/src/reports/dtos/get-estimate.dto.ts
@@ -1,10 +1,11 @@
+import { Transform } from 'class-transformer';
 import {
-  IsString,
-  IsNumber,
-  Min,
-  Max,
-  IsLongitude,
   IsLatitude,
+  IsLongitude,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
 } from 'class-validator';
 
 export class GetEstimateDto {
@@ -14,19 +15,23 @@ export class GetEstimateDto {
   @IsString()
   model: string;
 
+  @Transform(({ value }) => parseInt(value))
   @IsNumber()
   @Min(1930)
   @Max(new Date().getFullYear())
   year: number;
 
+  @Transform(({ value }) => parseInt(value))
   @IsNumber()
   @Min(0)
   @Max(1000000)
   mileage: number;
 
+  @Transform(({ value }) => parseFloat(value))
   @IsLongitude()
   lng: number;
 
+  @Transform(({ value }) => parseFloat(value))
   @IsLatitude()
   lat: number;
 }
