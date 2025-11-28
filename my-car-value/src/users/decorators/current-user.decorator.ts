9f7b397eb541f5010e54a222d8e419dd39c69d1c
Sarah O. <sarah.shofani@proton.me>
2025-11-28 10:07:47 -0300

    Formatting the Report Response

--- my-car-value/src/users/decorators/current-user.decorator.ts
+++ my-car-value/src/users/decorators/current-user.decorator.ts
@@ -3,10 +3,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const CurrentUser = createParamDecorator(
   (data: any, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
-    console.log('user:', request.session.user);
-    console.log('cookies:', context.switchToHttp().getRequest().headers.cookie);
-    console.log('session:', context.switchToHttp().getRequest().session);
-
-    return request.session.user;
+    return request.currentUser;
   },
 );
